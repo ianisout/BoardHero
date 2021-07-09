1674565220e2ab26be69f4cf9bbdcbf31eca2e09
Felipe <feemattioli@gmail.com>
2021-07-09 12:32:47 -0300

    cahnges

--- js/sidebars.js
+++ js/sidebars.js
@@ -6,3 +6,20 @@
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
 })()
+
+
+// let btn = document.querySelector("#btn");
+// let sidebar = document.querySelector(".sidebar");
+
+
+// btn.onclick = function() {
+//   sidebar.classList.toggle("active");
+//   if(btn.classList.contains("bx-menu")){
+//     btn.classList.replace("bx-menu" , "bx-menu-alt-right");
+//   }else{
+//     btn.classList.replace("bx-menu-alt-right", "bx-menu");
+//   }
+// }
+// searchBtn.onclick = function() {
+//   sidebar.classList.toggle("active");
+// }
