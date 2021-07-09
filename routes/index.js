9a24f586b652bc60a87e452965327ffefd6d7628
Nathalia Tescarollo <nathtescarollo@gmail.com>
2021-07-08 23:43:14 -0300

    adding partial files + router to task creation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index");
+});
+
+/* GET task creation function */
+router.get("/create-task", function (req, res, next) {
+  res.render("create-task");
 });
 
 module.exports = router;
