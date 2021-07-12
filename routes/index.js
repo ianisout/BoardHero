a7c254f04248ff0203efe1eb616ef1284bfabb6c
Nathalia Tescarollo <nathtescarollo@gmail.com>
2021-07-11 23:15:12 -0300

    adding routes to existing views + updating links

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,40 @@ router.get("/", function (req, res, next) {
   res.render("index");
 });
 
+/* GET home page */
+router.get("/index", function (req, res, next) {
+  res.render("index");
+});
+
 /* GET task creation function */
 router.get("/create-task", function (req, res, next) {
   res.render("create-task");
 });
+
+/* GET character or signup options page  */
+router.get("/char-login", function (req, res, next) {
+  res.render("char-login");
+});
+
+/* GET character creation page */
+router.get("/character-creation", function (req, res, next) {
+  res.render("character-creation");
+});
+
+/* GET signup page */
+router.get("/signup", function (req, res, next) {
+  res.render("signup");
+});
+
+/* GET login page */
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+/* GET reference page for sidebar and navbar components (TEST) */
+router.get("/homepage", function (req, res, next) {
+  res.render("homepage");
+});
+
 
 module.exports = router;
