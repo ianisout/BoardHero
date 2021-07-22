9191986db4b27460eb08be90341891fb6846b108
Nathalia Tescarollo <nathtescarollo@gmail.com>
2021-07-22 00:24:04 -0300

    upload file configuration with multer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const fs = require("fs");
+const os = require("os");
+const multer = require("multer");
+
+// get path of temp directory
+// example: 'C:/Users/natha/AppData/Local/Temp'
+const tempDir = os.tmpdir(); 
+const upload = multer({ dest: tempDir }); 
+
 /* GET home page */
 router.get("/", function (req, res, next) {
   res.render("index");
@@ -9,11 +18,6 @@ router.get("/", function (req, res, next) {
 /* GET home page */
 router.get("/index", function (req, res, next) {
   res.render("index");
-});
-
-/* GET task creation function */
-router.get("/create-task", function (req, res, next) {
-  res.render("create-task");
 });
 
 /* GET character or signup options page  */
@@ -43,6 +47,21 @@ router.get("/homepagecopy", function (req, res, next) {
 /* GET dashboard page */
 router.get("/dashboard", function (req, res, next) {
   res.render("dashboard");
+});
+
+/* GET task creation function */
+router.get("/create-task", function (req, res, next) {
+  res.render("create-task");
+});
+
+/* POST task creation form */
+router.post("/create-task", upload.array('task-files'), function (req, res, next) {
+  /* 
+
+    Handle form data and files...
+  
+  */
+  res.redirect("/dashboard");
 });
 
 /* GET view-task-details page */
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
