c67aab5d4f0dd65390ea5ab1e38c2b2d18f701f1
Nathalia Tescarollo <nathtescarollo@gmail.com>
2021-09-14 17:37:50 -0300

    task-create: add toggle for end_date and set min dates

--- public/javascripts/task-create.js
+++ public/javascripts/task-create.js
@@ -1,15 +1,33 @@
-const currentDate = new Date();
-let endDateCheckbox = document.querySelector("#enable_end_date");
-let endDateInput = document.querySelector("#end_date");
+const startDateInput = document.getElementById('start_date');
+const endDateInput = document.getElementById("end_date");
+const endDateCheckbox = document.getElementById("enable_end_date");
 
-document.getElementById('start_date').valueAsDate = currentDate;
+const currentDate = new Date();
+const yyyy = currentDate.getFullYear();
+let mm = currentDate.getMonth() + 1;
+let dd = currentDate.getDate();
+
+if (mm < 10) {
+  mm = '0' + mm;
+}
+
+if (dd < 10) {
+  dd = '0' + dd;
+}
+
+const today = yyyy + '-' + mm + '-' + dd;
+
+startDateInput.min = today;
+startDateInput.defaultValue = today;
+
+endDateInput.min = today;
 
 endDateCheckbox.addEventListener("click", ()=>{
   if (endDateCheckbox.checked) {
     endDateInput.disabled = false;
-    endDateInput.valueAsDate = currentDate;
+    endDateInput.defaultValue = today;
   } else {
     endDateInput.disabled = true;
-    endDateInput.valueAsDate = null;
+    endDateInput.defaultValue = "";
   }
 });
