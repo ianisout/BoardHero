1f503a85b59098ebcdec666315451ab8beb7151f
Ian <lsouza.ian@gmail.com>
2021-09-28 07:56:42 -0300

    feat: bring character elements to homepage

--- controllers/TypeOfElementController.js
+++ controllers/TypeOfElementController.js
@@ -5,3 +5,14 @@ exports.findAll = async () => {
   
   return elementTypes;
 }
+
+exports.findElementsById = async (arrayOfIds) => {
+  const arrayOfUrls = [];
+
+  for (let i = 0; i < arrayOfIds.length; i++) {
+    const element = await TypeOfElementModel.findElementsById(arrayOfIds[i])
+    arrayOfUrls.push(element.dataValues.image_path)
+  }
+
+  return arrayOfUrls;
+}
--- models/Character.js
+++ models/Character.js
@@ -1,12 +1,23 @@
-const { Character } = require("../database/models");
+const { Character } = require('../database/models');
 
 exports.createCharacter = async (user_id) => {
   const createdCharacter = await Character.create({
     char_level: 0,
     experience: 0,
     coins: 0,
-    user_id
+    user_id,
   });
 
   return createdCharacter.dataValues;
-}
+};
+
+exports.getCharacterByUserId = async (id) => {
+  const character = await Character.findOne({
+    where: {
+      user_id: id,
+    },
+    attributes: ['user_id', 'id'],
+  });
+
+  return character.dataValues;
+};
