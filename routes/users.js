8caf2d6b220ba8e43d64aaa72b6c0ff5f8797fb6
Felipe <feemattioli@gmail.com>
2021-10-31 20:45:44 -0300

    removing middlewares express validator

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,32 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const verifyLoggedUser = require("../middlewares/VerifyLoggedUser");
-const verifyNotLoggedUser = require("../middlewares/VerifyNotLoggedUser");
+const verifyLoggedUser = require('../middlewares/VerifyLoggedUser')
+const verifyNotLoggedUser = require('../middlewares/VerifyNotLoggedUser')
 
-const { validationResult } = require("express-validator");
-const signupValidations = require("../middlewares/signupValidation");
-const validationErrorMessage = require("../middlewares/validationMessage");
+const UserController = require('../controllers/UserController')
+const WorkspaceController = require('../controllers/WorkspaceController')
 
-const UserController = require("../controllers/UserController");
-const WorkspaceController = require("../controllers/WorkspaceController");
-
-const { log } = console;
+const { log } = console
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource')
+})
 
 /* GET signup page */
-router.get("/signup", verifyNotLoggedUser, function (request, response, next) {
-  const { CHARACTER_SET } = request.cookies;
-  response.cookie("CHARACTER_SET", CHARACTER_SET, { maxAge: 60000 });
-  response.render("signup");
-});
+router.get('/signup', verifyNotLoggedUser, function (request, response, next) {
+  const { CHARACTER_SET } = request.cookies
+  response.cookie('CHARACTER_SET', CHARACTER_SET, { maxAge: 60000 })
+  response.render('signup')
+})
 
 /* POST signup form */
-router.post("/signup", /*signupValidations, validationErrorMessage,*/ async function (request, response, next) {
-  console.log(validationErrorMessage)
+router.post('/signup', async function (request, response, next) {
   try {
-    const { CHARACTER_SET } = request.cookies;
-    const characterChoices = (CHARACTER_SET !== "undefined") ? JSON.parse(CHARACTER_SET) : undefined;
+    const { CHARACTER_SET } = request.cookies
+    const characterChoices =
+      CHARACTER_SET !== 'undefined' ? JSON.parse(CHARACTER_SET) : undefined
 
     const {
       first_name,
@@ -40,8 +36,8 @@ router.post("/signup", /*signupValidations, validationErrorMessage,*/ async func
       password,
       confirmPassword,
       position,
-      company,
-    } = request.body;
+      company
+    } = request.body
 
     const userCreated = await UserController.createUser({
       first_name,
@@ -53,88 +49,88 @@ router.post("/signup", /*signupValidations, validationErrorMessage,*/ async func
       position,
       company,
       characterChoices
-    });
+    })
 
     if (userCreated) {
-      const isAdmin = true;
-      const workspaceName = `${first_name}'s Workspace`;
+      const isAdmin = true
+      const workspaceName = `${first_name}'s Workspace`
 
       const workspaceCreated = await WorkspaceController.createWorkspace({
         workspace: {
-          name: workspaceName,
+          name: workspaceName
         },
         userId: userCreated.id,
-        isAdmin: isAdmin,
-      });
+        isAdmin: isAdmin
+      })
 
       const userSession = {
         ...userCreated,
         workspaces: [workspaceCreated],
         activeWorkspace: {
           id: workspaceCreated.id,
-          isAdmin: isAdmin,
-        },
-      };
+          isAdmin: isAdmin
+        }
+      }
 
-      request.session.user = userSession;
+      request.session.user = userSession
 
-      response.clearCookie("CHARACTER_SET");
-      response.status(201).redirect("/homepage");
+      response.clearCookie('CHARACTER_SET')
+      response.status(201).redirect('/homepage')
     }
   } catch (error) {
-    console.log(error);
-    return response.render('signup');
+    console.log(error)
+    return response.render('signup')
   }
-});
+})
 
 /* GET login page */
-router.get("/login", verifyNotLoggedUser, function (req, res, next) {
-  res.render("login");
-});
+router.get('/login', verifyNotLoggedUser, function (req, res, next) {
+  res.render('login')
+})
 
 /* POST login form */
-router.post("/login", async function (req, res, next) {
+router.post('/login', async function (req, res, next) {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
-    const userLogged = await UserController.loginUser({ email, password });
+    const userLogged = await UserController.loginUser({ email, password })
 
     if (userLogged) {
-      const defaultWorkspace = userLogged.workspaces[0];
+      const defaultWorkspace = userLogged.workspaces[0]
 
       const userSession = {
         ...userLogged,
         activeWorkspace: {
           id: defaultWorkspace.id,
-          isAdmin: true,
-        },
-      };
+          isAdmin: true
+        }
+      }
 
-      req.session.user = userSession;
-      log(req.session);
+      req.session.user = userSession
+      log(req.session)
 
-      res.status(201).redirect("/homepage");
+      res.status(201).redirect('/homepage')
     }
   } catch (error) {
-    console.log(error);
-    return res.render('login', { error: error.message });
+    console.log(error)
+    return res.render('login', { error: error.message })
   }
-});
+})
 
 /* GET logout */
-router.get("/logout", function (req, res, next) {
-  delete req.session.user;
-  res.status(201).redirect("/");
-});
+router.get('/logout', function (req, res, next) {
+  delete req.session.user
+  res.status(201).redirect('/')
+})
 
 /* GET forgot-password page */
-router.get("/forgot-password", function (req, res, next) {
-  res.render("forgot-password");
-});
+router.get('/forgot-password', function (req, res, next) {
+  res.render('forgot-password')
+})
 
 /* GET user settings page */
-router.get("/settings", verifyLoggedUser, function (req, res, next) {
-  res.render("settings", { user: req.session.user });
-});
+router.get('/settings', verifyLoggedUser, function (req, res, next) {
+  res.render('settings', { user: req.session.user })
+})
 
-module.exports = router;
+module.exports = router
