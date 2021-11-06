41442a589bfc0f6a1f9cda48e102a3a40892644c
Ian <lsouza.ian@gmail.com>
2021-11-06 16:16:36 -0300

    feat: change background image

---
+++ public/javascripts/changeBackground.js
@@ -0,0 +1,32 @@
+function setBackgroundSelectionDiv() {
+  const optionsDiv = document.querySelector('.background-selection');
+  optionsDiv.style.display = "unset"  
+}
+
+function setBackgroundFromSystem(url) {
+  const homeSection = document.querySelector('.home-section');
+  homeSection.style.backgroundImage = `url(${url})`;
+  document.querySelector('.background-selection').style.display = "none";
+  localStorage.setItem('background_image', url);
+}
+
+function setBackgroundFromStorage() {
+  const homeSection = document.querySelector('.home-section');
+  const url = localStorage.getItem("background_image");
+  homeSection.style.backgroundImage = `url(${url})`;
+}
+
+async function getImage () {
+  try {
+    await axios.get('https://picsum.photos/1920/1080/?blur=2')
+    .then((res) => {
+      const backgroundUrl = res.request.responseURL;
+      const homeSection = document.querySelector('.home-section')
+      homeSection.style.backgroundImage = `url('${backgroundUrl}')`;
+      document.querySelector('.background-selection').style.display = "none";
+      localStorage.setItem('background_image', backgroundUrl);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
